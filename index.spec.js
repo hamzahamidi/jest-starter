d0102ded27dd721770d12c5a57b0964c0d077b1e
Hamza Hamidi <hamidihamzacg@gmail.com>
2019-12-26 15:03:57 +0100

    Fix(Index.js): Correct tests

--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,11 @@
 import { sum } from './index';
 
 test('adds 1 + 2 to equal 3', () => {
-  const result = sum(4,5);
+  const result = sum(1,2);
   expect(result).toBe(3);
 });
 
 test('adds 5 + 8 to equal 13', () => {
   const result = sum(5,8);
-  expect(result).toBe(3);
+  expect(result).toBe(13);
 });
